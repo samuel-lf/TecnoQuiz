95fca8096bfb05b4281a0482124932fa98ce427a
Samuel Leonel <samueleonelfreitas@hotmail.com>
2021-01-26 16:41:18 -0300

    Estilização da página do quiz e exibição do nome

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import { route } from 'next/dist/next-server/server/router';
+import db from '../db.json';
+import Footer from '../src/components/Footer';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import GitHubCorner from '../src/components/GithubCorner';
+import Widget from '../src/components/Widget';
 
-export default function QuizPage() {
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState(router.query.name);
+
   return (
-    <h1>teste</h1>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>Perguntas</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <h1>{name}</h1>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/samuel-lf/TecnoQuiz" />
+    </QuizBackground>
   );
 }
