8e172c1ff377169568039574bd860cb6162c5173
Samuel Leonel <samueleonelfreitas@hotmail.com>
2021-01-25 22:11:53 -0300

    Ajustes de target no footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -36,7 +36,7 @@ export default function Footer(props) {
         {' '}
         a
         {' '}
-        <a href="https://www.alura.com.br/">
+        <a href="https://www.alura.com.br/" target="_blank">
           <span>Imersão React da Alura</span>
         </a>
       </p>
