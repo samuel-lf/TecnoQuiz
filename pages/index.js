51132f9e28c8697e89f769a5cdcf83e587f379d1
Samuel Leonel <samueleonelfreitas@hotmail.com>
2021-01-25 22:02:35 -0300

    Criação das seções de widget

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,40 @@
+import styled from 'styled-components'
 import db from '../db.json';
 import QuizBackground from '../src/components/QuizBackground'
+import Widget from '../src/components/Widget'
 
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>Dê uma olhada em outros quizes incriveis que o pessoal da Imersão fez:</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
     </QuizBackground>
   )
 }
