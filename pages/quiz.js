51346e2a6f9da36187fa706de576d59c5bf76a7a
Samuel Leonel <samueleonelfreitas@hotmail.com>
2021-01-27 15:43:11 -0300

    Criação da página de quiz

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { useRouter } from 'next/router';
-import { route } from 'next/dist/next-server/server/router';
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
 import db from '../db.json';
 import Footer from '../src/components/Footer';
 import QuizBackground from '../src/components/QuizBackground';
@@ -8,23 +7,115 @@ import QuizContainer from '../src/components/QuizContainer';
 import QuizLogo from '../src/components/QuizLogo';
 import GitHubCorner from '../src/components/GithubCorner';
 import Widget from '../src/components/Widget';
+import Button from '../src/components/Button';
+
+function LoadingWidget() {
+  return (
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
+
+      <Widget.Content>
+        [Desafio do Loading]
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+function QuestionWidget({
+  question, totalQuestions, questionIndex, onSubmit,
+}) {
+  const questionId = `question__${questionIndex}`;
+
+  return (
+    <Widget>
+      <Widget.Header>
+        <h3>
+          {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+        </h3>
+      </Widget.Header>
+      <img
+        alt="Descrição"
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+        src={question.image}
+      />
+      <Widget.Content>
+        <h2>{question.title}</h2>
+        <p>{question.description}</p>
+        <form onSubmit={(event) => {
+          event.preventDefault();
+          onSubmit();
+        }}
+        >
+          {
+            question.alternatives.map((alternative, index) => {
+              const alternativeId = `alternative__${index}`;
+              return (
+                <Widget.Topic as="label" htmlFor={alternativeId} key={alternativeId}>
+                  <input name={questionId} id={alternativeId} type="radio" />
+                  {alternative}
+                </Widget.Topic>
+              );
+            })
+          }
+          <Button type="submit">
+            Confirmar
+          </Button>
+        </form>
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+const screenStates = {
+  Loading: 'Loading',
+  Quiz: 'Quiz',
+  Result: 'Result',
+};
 
 export default function Home() {
-  const router = useRouter();
-  const [name, setName] = useState(router.query.name);
+  const [screenState, setScreenState] = useState(screenStates.Loading);
+  const totalQuestions = db.questions.length;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const questionIndex = currentQuestion;
+  const question = db.questions[questionIndex];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.Quiz);
+    }, 1 * 1000);
+  }, []);
+
+  function handleSubmitQuiz() {
+    const nextQuestion = questionIndex + 1;
+    if (nextQuestion < totalQuestions) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setScreenState(screenStates.Result);
+    }
+  }
 
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>Perguntas</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <h1>{name}</h1>
-          </Widget.Content>
-        </Widget>
+        {
+          screenState === screenStates.Quiz && (
+            <QuestionWidget
+              question={question}
+              totalQuestions={totalQuestions}
+              questionIndex={questionIndex}
+              onSubmit={handleSubmitQuiz}
+            />
+          )
+        }
+        { screenState === screenStates.Loading && <LoadingWidget /> }
+        { screenState === screenStates.Result && <p>Resultado</p> }
         <Footer />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/samuel-lf/TecnoQuiz" />
